2f99367f23bc0280287ac84cd1da739b06d9796a
Mariusbergstein <144114880+Mariusbergstein@users.noreply.github.com>
2023-09-14 16:40:40 +0200

    Add files via upload

--- Step2.py
+++ Step2.py
@@ -1,158 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  5 10:42:27 2023
-
-@author: benp.
-"""
-
-# importing
-#from IPython import get_ipython
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-pd.options.mode.chained_assignment = None
-
-# housekeeping
-#get_ipython().magic('reset -sf')
-plt.close('all')
-
-# =============================================================================
-# STEP 1
-# =============================================================================
-
-# --- TASK 1.1 ---
-
-# getting data
-riverNodes = pd.read_csv('riverNodes.csv', sep=",", decimal = ".")
-distances = pd.read_csv('distances.csv', sep=",", decimal=".")
-#renaming danish words
-new_column_name = 'Vandmaengd'
-distances = distances.rename(columns={distances.columns[12]: new_column_name})
-new_column_name2 = 'Bygvaerkst'
-distances = distances.rename(columns={distances.columns[7]: new_column_name2})
-# removing unnecessary data
-riverNodes.rename(columns={"vandfoering": "water flow (m^3/s)"}, inplace=True)
-distances.rename(columns={"BI-5 (kg)": "Biological oxygen demand, 5 days (kg)"}, inplace=True)
-distances = distances.drop(['Reduceret'],axis=1)
-distances = distances.drop(['Udlednings'],axis=1)
-distances = distances.drop(['Bygvaerkst'],axis=1)
-distances = distances.drop(['Godkendels'],axis=1)
-distances = distances.drop(['Ejer'],axis=1)
-distances = distances.drop(['Idriftsat'],axis=1)
-distances = distances.drop(['Nedlagt'],axis=1)
-distances = distances.drop(['Antal over'],axis=1)
-
-meandistm = distances.loc[:,'HubDist'].mean()*1000
-
-# There are 27 CSO structures along the stream, located along the banks of the stream from 
-# the outlet to Lyngby Lake to the Oeresund. They are located, on average, 103 m
-# from the banks of the stream. 
-
-annualCSOwaterflow = distances.loc[:,'Vandmaengd'].sum()
-pollutants = distances.iloc[:,7:10].sum()
-pollutants = pollutants.sum()
-
-# 289666 m^3 of water and 12744 kg of pollutants were discharged into the stream. 
-# The UF01001 CSO in Ravnholm saw the
-# largest number of overflows, and the Ly R16 CSO saw the largest volume of water. 
-# Ly R16 is located close to a bridge near the outlet to Lyngby Lake, while UF01001 
-# is located near a highway overpass, closer to the ocean. We would thus expect the
-# major negative effects to occur closer to the ocean, where pollutants have
-# accumulated as they have flowed through the stream. 
-
-meanflow = riverNodes.loc[:,'water flow (m^3/s)'].mean()
-months = ['januar','februar','marts','april','maj','juni','juli','august','september','oktober','november','december']
-
-
-# Convert the 'maaned' column to strings
-riverNodes['maaned'] = riverNodes['maaned'].astype(str)
-#looping through the month column, pulling out values for each month, 
-# calculating the average, and then putting it in the empty array I made#
-monthflows=pd.DataFrame(np.zeros([12,2],dtype=float),columns=['month','flow (m^3/s)'])
-for i in range(len(months)):
-    monthflows['month'][i] = months[i]
-    idx = riverNodes['maaned'].str.contains(months[i])
-    data = riverNodes[idx]
-    data = data.reset_index(drop=True)
-    monthflows['flow (m^3/s)'][i] = data['water flow (m^3/s)'].mean()
-
-
-# The average flow of the river in Moelle Aa is 0.24 m3/s. Highest month is Jan, lowest August
-
-# idxflowLyngby = riverNodes['beregningspunktlokalid'].str.contains('Novana_Model_MOELLEAA_DK1_3687')
-# flowLyngby = riverNodes[idxflowLyngby]
-# flowLyngby = flowLyngby.reset_index(drop=True)
-
-# years = list(range(1990, 2020))
-# yearsLyngby=pd.DataFrame(np.zeros([30,2],dtype=float),columns=['year','flow (m^3/s)'])
-# for i in range(len(years)):
-#     yearsLyngby['year'][i] = years[i]
-#     idx = flowLyngby['aar'] == years[i]
-#     data = flowLyngby[idx]
-#     data = data.reset_index(drop=True)
-#     yearsLyngby['flow (m^3/s)'][i] = data.loc[:,'water flow (m^3/s)'].sum()
-#     averageannual = yearsLyngby['flow (m^3/s)'].mean()
-
-# --- TASK 1.2 ---
-
-# importing in annual data and selecting for the outlet from the lake
-riverAnnual = pd.read_csv('yearlyvalues.csv', sep=",", decimal = ".")
-riverAnnual = riverAnnual.tail(-1)
-idxflowLyngby2 = riverAnnual['beregningspunktlokalid'].str.contains('Novana_Model_MOELLEAA_DK1_13500')
-flowLyngby2 = riverAnnual[idxflowLyngby2]
-flowLyngby2 = flowLyngby2.reset_index(drop=True)
-
-# finding the average annual flow in m^3/s and convering to m^3/year
-avgyear = flowLyngby2['vandfoering'].mean()*3600*24*365
-fraction = annualCSOwaterflow/avgyear # 1.26% of the water entering the Oeresund is coming from the CSOs
-
-# getting nitrogen data into Python for Lyngby lake
-lakeData = pd.DataFrame(np.zeros([12,2],dtype=float),columns=['year','nitrogen (mg/L)'])
-years = list(range(2002, 2015))
-nmgL = [0.07, 0.0155, 0.02, 0.15, 0.03, 0.04, 0.173, 0.25, 0.01, 0.3, 0.15, 0.075, 0]
-for i in range(len(years)):
-     lakeData['year'][i] = years[i]
-     lakeData['nitrogen (mg/L)'][i] = nmgL[i]
-
-# converting the kg of nitrogen passing through the CSOs to mg/L
-CSOnitromg = distances.loc[:,"Total-N (k"].sum()*1000000
-CSOmgnitroperL = CSOnitromg/(annualCSOwaterflow*1000)
-
-# looping through CSO dataset to find nitrogen contribution from each CSO
-CSOnitro = pd.DataFrame(np.zeros([27,2],dtype=float),columns=['CSO','nitrogen (mg/L)'])
-for i in range(len(distances)):
-    CSOnitro['CSO'][i] = distances['Navn'][i]
-    kgnitro = distances['Total-N (k'][i]
-    mgnitro = kgnitro*1000000
-    Lflow = distances['Vandmaengd'][i]*1000
-    # avoid zero division
-    if (Lflow == 0):
-        Lflow += 1e-8
-    CSOnitro['nitrogen (mg/L)'][i] = mgnitro/Lflow
-meanNinlake = lakeData['nitrogen (mg/L)'].mean() # mg/L
-flowfromlakeL = avgyear*1000 # L/yr
-nitrofromlake = (meanNinlake*flowfromlakeL)*(1e-6) # kg/yr
-nitrofromCSOs = distances['Total-N (k'].sum() # kg
-
-
-# major source of nitrogen is CSOs and we know the specific ones. 
-
-# --- TASK 1.3 ---
-
-ibtypetal = 1.7 # in mcg/L
-ibpercentile = 1.8 # in mcg/L, using median to represent most common value
-
-# 2 units different, we will pick tomorrow (pg 61-62)
-
-# =============================================================================
-# STEP 2
-# =============================================================================
-riverNodes = riverNodes.tail(-1)
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+import os
+os.chdir('C:/Users/mariu/Documents/Python Scripts/Environmental modelling/Assignment 1')
 pd.options.mode.chained_assignment = None
 
 # housekeeping
@@ -162,6 +13,7 @@ plt.close('all')
 # getting data
 riverNodes = pd.read_csv('riverNodes.csv', sep=",", decimal = ".")
 distances = pd.read_csv('distances.csv', sep=",", decimal=".")
+
 #renaming danish words
 new_column_name = 'Vandmaengd'
 distances = distances.rename(columns={distances.columns[12]: new_column_name})
@@ -170,11 +22,21 @@ distances = distances.rename(columns={distances.columns[7]: new_column_name2})
 # removing unnecessary data
 riverNodes.rename(columns={"vandfoering": "water flow (m^3/s)"}, inplace=True)
 distances.rename(columns={"BI-5 (kg)": "Biological oxygen demand, 5 days (kg)"}, inplace=True)
-riverNodes.head(5)
+
+# %%
+#riverNodes.head(5)
+
+# %%
 riverNodes['beregningspunktlokalid'].unique()
+
+# %%
 distances.head(5)
+
+# %%
 # Filter rows where 'registreringfra' contains either 'Novana_Model_MOELLEAA_DK1_13500' or 'Novana_Model_MOELLEAA_DK1_3687'
 idxMoelleAA = riverNodes['beregningspunktlokalid'].str.contains('MOELLEAA')
+
+# %%
 riverNodes = riverNodes[idxMoelleAA]
 riverNodes = riverNodes.reset_index()
 riverNodesYY = riverNodes[riverNodes["aar"]==2011]
@@ -190,25 +52,175 @@ idxUp_num = idxUp_num[0]
 idxDown_num = idxDown_num[0]
 indexdiff= abs(idxUp_num - idxDown_num)
 
+
+# %%
 idxUpDown = idxUp
+
+# %%
 for i in range(idxUp_num, idxDown_num+1):
     idxUpDown[i] = True
+
+# %%
 riverQ = riverNodesMM[idxUpDown]
+
+# %%
 riverQ = riverQ[['water flow (m^3/s)', 'beregningspunktlokalid', 'X', 'Y']]
+
+# %%
 riverQ.reset_index()
 riverQ['distance'] = np.NaN
 riverQ['CSOflow'] = np.NaN
 
+# %%
+riverC = riverQ[['beregningspunktlokalid', 'X', 'Y', 'distance']]
+riverC = riverC.reset_index(drop=True)
+EQS_exc= riverC.copy()
+
+# %%
+StringName=riverQ['beregningspunktlokalid'].str.split('_').str[-1]
+StringName = StringName.astype(float)
+
+# %%
+type(StringName)
+
+# %%
+riverQ["distance"]= StringName - 3687
+riverQ = riverQ.reset_index(drop=True)
+
+# %%
+riverC['concentration'] = 0
+
+# %%
+# starting the simple advection-dilution model 
+riverC['concentration'][0] = 0.2*(10**(-6)) # initial concentration in the stream
+CSO_conc = 1.7*(10**(-6)) # (g/L) assign the MP concentration in the CSO water
+theta = 2.31 # %
+t_CSO = 4.3 # hr
 
 
+# %% 
+CSOname=pd.DataFrame(np.zeros([len(riverQ),1],dtype=float),columns=['name'])
+
+# # looping from 1 to len-1 instead of 2 to len (appendix)
+# for i in range(1,len(riverQ)-1):
+#     csoBool = distances['HubName'].eq(riverQ["beregningspunktlokalid"].iloc[i]) # comparing each value
+#     isThereCSO = csoBool.sum() == 1 # check if there is a CSO
+
+# %%
+
+CSOname=pd.DataFrame(np.zeros([len(riverQ),1],dtype=float),columns=['name'])
+for i in range(1,len(riverQ)-1):
+    csoBool = distances['HubName'].eq(riverQ["beregningspunktlokalid"].iloc[i])
+    isThereCSO = csoBool.sum() == 1 # check if there is a CSO
+    if isThereCSO: 
+        CSOname['name'][i] = riverQ["beregningspunktlokalid"].iloc[i]
+    idxCSO = (CSOname[CSOname['name'] != 0]).index
+    namesCSO = CSOname[CSOname['name'] != 0]
+
+# Create an empty list to store the indices where the string appears
+indices_where_appears = []
+
+# Iterate through each string in namesCSO and check if it appears in distances['HubName']
+for cso_name in namesCSO['name']:
+    # Use str.contains to create a boolean mask
+    mask = distances['HubName'].str.contains(cso_name)
+    
+    # Use the mask to get the indices where the string appears
+    matching_indices = distances[mask].index.tolist()
+    
+    # Add the matching indices to the list
+    indices_where_appears.extend(matching_indices)
+
+# The list indices_where_appears now contains the indices in distances where the strings from namesCSO appear.
+
+    
+    if len(idxCSO) > 0:
+        CSO_flux = 0 #create an empty value for the MP flux from the CSO
+        CSO_Qtot = 0
+        for j in range(0,len(idxCSO)-2): #loop over the connected CSOs
+            V_CSO = distances["Vandmaengd"][idxCSO[j]]*theta # calculate the volume of the single discharge event
+            Q_CSO = V_CSO/t_CSO # calculate the flow of the single discharge event
+            CSO_flux = CSO_flux+Q_CSO*CSO_conc
+            CSO_Qtot = CSO_Qtot+Q_CSO
+        riverQ['CSOflow'][i] = riverQ['CSOflow'][i-1]+CSO_Qtot
+        # calculate concentration for the node
+        riverC['concentration'][i] = (riverC['concentration'][i-1]*(riverQ['water flow (m^3/s)'][i-1]+
+        riverQ['CSOflow'][i-1])+ CSO_flux)/ (riverQ['water flow (m^3/s)'][i]+
+        riverQ['CSOflow'][i])
+    else: # there are no CSO connected to the node
+        # calculate flow added to the river by the CSO (no addition)
+        riverQ['CSOflow'][i]= riverQ['CSOflow'][i-1]
+        # calculate concentration for the node
+        riverC['concentration'][i] =riverC['concentration'][i-1]*(riverQ['water flow (m^3/s)'][i-1]+
+        riverQ['CSOflow'][i-1])/ (riverQ['water flow (m^3/s)'][i]+
+        riverQ['CSOflow'][i])
+                                  
+# EQS_exc= riverC["conc"]>EQS_exc
 
 
+# %%
 
+# CSOname=pd.DataFrame(np.zeros([len(riverQ),1],dtype=float),columns=['name'])
 
+# for i in range(2,len(riverQ)):
+#     csoBool = riverQ['beregningspunktlokalid'].eq(distances['HubName'].iloc[i])
 
+#     isThereCSO = csoBool.sum() == 1 # check if there is a CSO
+#     if isThereCSO: 
+#         CSOname['name'][i] = riverQ["beregningspunktlokalid"].iloc[i]
+#     idxCSO = (CSOname[CSOname['name'] != 0]).index
+#     if len(idxCSO) > 0:
+#         CSO_flux = 0 #create an empty value for the MP flux from the CSO
+#         CSO_Qtot = 0
+#         for j in range(0,len(idxCSO)-2): #loop over the connected CSOs
+#             print(idxCSO[j])
+#             V_CSO = distances["Vandmaengd"][idxCSO[j]]*theta # calculate the volume of the single discharge event
+#             Q_CSO = V_CSO/t_CSO # calculate the flow of the single discharge event
+#             CSO_flux = CSO_flux+Q_CSO*CSO_conc
+#             CSO_Qtot = CSO_Qtot+Q_CSO
+#         riverQ['CSOflow'][i] = riverQ['CSOflow'][i-1]+CSO_Qtot
+#         # calculate concentration for the node
+#         riverC['concentration'][i] = (riverC['concentration'][i-1]*(riverQ['water flow (m^3/s)'][i-1]+
+#         riverQ['CSOflow'][i-1])+ CSO_flux)/ (riverQ['water flow (m^3/s)'][i]+
+#         riverQ['CSOflow'][i])
+#     else: # there are no CSO connected to the node
+#         # calculate flow added to the river by the CSO (no addition)
+#         riverQ['CSOflow'][i]= riverQ['CSOflow'][i-1]
+#         # calculate concentration for the node
+#         riverC['concentration'][i] =riverC['concentration'][i-1]*(riverQ['water flow (m^3/s)'][i-1]+
+#         riverQ['CSOflow'][i-1])/ (riverQ['water flow (m^3/s)'][i]+
+#         riverQ['CSOflow'][i])
+                                  
+# EQS_exc= riverC["conc"]>EQS_exc
 
-
-
-
-
-
+        
+    
+# %%
+# for i in range(2,len(riverQ)):
+#     csoBool = distances['HubName'].eq(riverQ["beregningspunktlokalid"].iloc[i])
+#     isThereCSO = csoBool.sum() == 1 # check if there is a CSO
+#     if isThereCSO:
+#         idxCSO = csoBool[csoBool == True].index
+#         #print(distances.iloc[idxCSO])
+#         CSO_flux=0 #create an empty value for the MP flux from the CSO
+#         CSO_Qtot=0 #create an empty value for the total CSO flow added to the river
+#         for j in range(1,len(idxCSO)): #loop over the connected CSOs
+#             V_CSO= distances["Vandmaengd"][idxCSO[j]]*theta # calculate the volume of the single discharge event
+#             Q_CSO=V_CSO/t_CSO # calculate the flow of the single discharge event
+            
+#             CSO_flux=CSO_flux+Q_CSO*CSO_conc
+#             CSO_Qtot= CSO_Qtot+Q_CSO
+#         riverQ['CSOflow'][i]= riverQ['CSOflow'][i-1]+CSO_Qtot
+#         # calculate concentration for the node
+#         riverC['concentration'][i]=(riverC['concentration'][i-1]*(riverQ['water flow (m^3/s)'][i-1]+
+#         riverQ['CSOflow'][i-1])+ CSO_flux)/ (riverQ['water flow (m^3/s)'][i]+
+#         riverQ['CSOflow'][i])
+#     else: # there are no CSO connected to the node
+#         # calculate flow added to the river by the CSO (no addition)
+#         riverQ['CSOflow'][i]= riverQ['CSOflow'][i-1]
+#         # calculate concentration for the node
+#         riverC['concentration'][i] =riverC['concentration'][i-1]*(riverQ['water flow (m^3/s)'][i-1]+
+#         riverQ['CSOflow'][i-1])/ (riverQ['water flow (m^3/s)'][i]+
+#         riverQ['CSOflow'][i])
+   
+# # EQS_exc= riverC["conc"]>EQS_exc
