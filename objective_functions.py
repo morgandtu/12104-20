9c91f49c3e534f455bf27b80c363f72885ca6bac
morgandtu <144114442+morgandtu@users.noreply.github.com>
2023-11-14 10:32:58 +0100

    Add files via upload

---
+++ objective_functions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 08:59:20 2023
+
+@author: benp.
+"""
+
+import numpy as np
+
+def RMSE(obs_data,sim_data):
+    err = (sim_data-obs_data)**2 # squared error vector (vector)
+    objFun = (np.mean(err))**.5 # calculate RMSE (a single number)
+    return objFun
+
+# new objective function
+def invMSE(obs_data,sim_data):
+    err=(sim_data-obs_data)**2 # error vector
+    objFun=np.mean(err) # calculate MSE
+    objFun=1/objFun # calculate inverse of MSE
+    return objFun
